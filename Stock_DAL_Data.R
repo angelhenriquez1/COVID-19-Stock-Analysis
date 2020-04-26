affc278cc4aecd5e8f914e5103d2acfa428866c4
Angel Henriquez <46985278+angelhenriquez1@users.noreply.github.com>
2020-04-25 19:56:06 -0700

    Update Stock_DAL_Data.R

--- Stock_DAL_Data.R
+++ Stock_DAL_Data.R
@@ -6,8 +6,12 @@ library(tidyverse)
 library(readxl)
 library(writexl)
 
-# stock data from 2014
+# DAL stock data from 2014
 Stock_2014_DAL <- read_excel("Stock_2014_DAL.xlsx")
+# COVID-19 and Southwest Data
+COVID19_SOUTHWEST <- read_excel("COVID19-SOUTHWEST.xlsx")
+# Ebola virus dataset
+Ebola_Southwest_Data <- read_excel("Ebola-Southwest-Data.xlsx")
 
 # adding 2014 year label
 names(Stock_2014_DAL)[2] <- "Open_14"
@@ -44,9 +48,7 @@ Stock_2020_DAL <- Stock_2020_DAL %>%
 # merging based on id
 DAL_Stock <- merge(Stock_2014_DAL, Stock_2020_DAL, by = "id")
 
-write_xlsx(DAL_Stock, "DAL_Stock.xlsx")
-write.csv(DAL_Stock, "DAL_Stock.csv")
-
+# DAL Stock of 2014 and 2020
 plot(DAL_Stock$Open_14,
      main="Stock Prices for Ebola and COVID-19",
      sub = "Beginning on the First Officially Declared Case",
@@ -55,3 +57,56 @@ plot(DAL_Stock$Open_14,
      col="blue", 
      ylim=c(10, 42))
 lines(DAL_Stock$Open_20, col="red")
+legend("bottomleft",
+       c("2020","2014"),
+       fill=c("red","blue")
+)
+
+# Difference between years
+DAL_Stock$Change_in_Stocks <- DAL_Stock$Open_20 - DAL_Stock$Open_14
+
+# plot of difference in DAL stocks between 2014 and 2020
+plot(DAL_Stock$Change_in_Stocks, 
+     main = "Difference in DAL Stocks Between 2014 and 2020", 
+     ylab = "Stock Difference",
+     xlab = "Days Since Beginning of Pandemics")
+lines(DAL_Stock$Change_in_Stocks, 
+      col="red")
+
+# Excel file of DAL Stocks
+write_xlsx(DAL_Stock, "DAL_Stock.xlsx")
+
+# CSV file of DAL Stocks
+write.csv(DAL_Stock, "DAL_Stock.csv")
+
+# Southwest
+COVID19_SOUTHWEST <- COVID19_SOUTHWEST %>%
+  mutate(id = row_number())
+Ebola_Southwest_Data <- Ebola_Southwest_Data %>%
+  mutate(id = row_number())
+Stock_2014_DAL <- Stock_2014_DAL %>%
+  mutate(id = row_number())
+Stock_2020_DAL <- Stock_2020_DAL %>%
+  mutate(id = row_number())
+
+# entire dataset
+calhacksdata <- merge(COVID19_SOUTHWEST, Ebola_Southwest_Data, by = c("id")) 
+calhacksdata <- merge(calhacksdata, DAL_Stock, by = c("id"))
+
+# Graph of Southwest data for COVID-19
+plot(COVID19_SOUTHWEST$Date, COVID19_SOUTHWEST$Close, xlab = "Date",ylab = " Closing Stock Price", Main = "Southwest Stock Price Over Span of Covid-19")
+lines.default(COVID19_SOUTHWEST$Date, COVID19_SOUTHWEST$Close)
+lm(COVID19_SOUTHWEST$Date~ COVID19_SOUTHWEST$Close)
+abline(lm(COVID19_SOUTHWEST$Date~ COVID19_SOUTHWEST$Close))
+
+# Graph of Southwest data for Ebola
+plot(Ebola_Southwest_Data$Date, Ebola_Southwest_Data$Close, xlab = "Date",ylab = "Closing Stock Price", Main = "Southwest Stock Price Over Span of Ebola")
+lines.default(Ebola_Southwest_Data$Date, Ebola_Southwest_Data$Close)
+lm(Ebola_Southwest_Data$Date~ Ebola_Southwest_Data$Close)
+abline(lm(Ebola_Southwest_Data$Date~ Ebola_Southwest_Data$Close))
+
+# Excel file of entire dataset
+write_xlsx(calhacksdata, "calhacksdata.xlsx")
+
+# CSV file of entire dataset
+write.csv(calhacksdata, "calhacksdata.csv")
